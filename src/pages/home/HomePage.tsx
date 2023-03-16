ef00291c60bae45581a05fea31379ef39178fe60
gucudmitriy <dmitiy.g@helium10.com>
2023-03-16 20:13:26 +0200

    test

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export const HomePage = () => {
   const [count, setCount] = useState(0);
+  const num = 1;
 
   useEffect(() => {
     setCount(count + 1);
