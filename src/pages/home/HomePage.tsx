558ccd2ff5f51e4cb142ff19ece3301c993c15cc
gucudmitriy <dmitiy.g@helium10.com>
2023-03-16 20:12:05 +0200

    test

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,10 @@
+import { useEffect, useState } from "react";
+
 export const HomePage = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(count + 1);
+  }, []);
   return <div>Home Page</div>;
 };
